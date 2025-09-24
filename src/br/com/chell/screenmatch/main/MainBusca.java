b64a1bb3f94f7a334d811c1e61f9db00ff18bd30
Chell <rodriguessnts@outlook.com>
2025-09-24 12:15:46 -0300

    Refatorando main

--- src/br/com/chell/screenmatch/main/MainBusca.java
+++ src/br/com/chell/screenmatch/main/MainBusca.java
@@ -1,16 +1,11 @@
 package br.com.chell.screenmatch.main;
 
 import br.com.chell.screenmatch.api.InicializadorDebugApi;
-import br.com.chell.screenmatch.api.OrganizadorApi;
-import br.com.chell.screenmatch.api.OrganizadorHttp;
 
 import java.io.IOException;
 
 public class MainBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
-
-        OrganizadorApi filmeLink = new OrganizadorApi();
-        OrganizadorApi api = new OrganizadorApi();
 
         InicializadorDebugApi iniciar = new InicializadorDebugApi();
 
