63360033a1b4c00cfda32589a68ca171456c85ad
Chell <rodriguessnts@outlook.com>
2025-09-24 01:19:42 -0300

    Criando o sistema de requeste e response

---
+++ src/br/com/chell/screenmatch/api/OrganizadorApi.java
@@ -0,0 +1,36 @@
+package br.com.chell.screenmatch.api;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Scanner;
+
+public class OrganizadorApi {
+    private String filmeLink;
+    private String api;
+
+    Scanner s = new Scanner(System.in);
+
+    public String getApi() {
+        return api;
+    }
+
+    public String getFilmeLink() {
+        return filmeLink;
+    }
+
+    public void pegaFilmeLink(){
+        System.out.print("Digite o nome do filme: ");
+        String filmeDigitado = s.nextLine();
+        try {
+            this.filmeLink = URLEncoder.encode(filmeDigitado, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            this.filmeLink = filmeDigitado.replace(" ", "+");
+        }
+    }
+
+    public void pegaApi() {
+        System.out.print("Digite sua APIKey do OMDb: ");
+        api = s.next();
+    }
+}
--- src/br/com/chell/screenmatch/main/MainBusca.java
+++ src/br/com/chell/screenmatch/main/MainBusca.java
@@ -1,4 +1,32 @@
 package br.com.chell.screenmatch.main;
 
+import br.com.chell.screenmatch.api.OrganizadorApi;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class MainBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        OrganizadorApi filmeLink = new OrganizadorApi();
+        OrganizadorApi api = new OrganizadorApi();
+
+        filmeLink.pegaFilmeLink();
+        //System.out.println(filmeLink.getFilmeLink()); debug
+
+        api.pegaApi();
+        //System.out.println(api.getApi()); debug
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()                                                  //ac4c9a6
+                .uri(URI.create("https://www.omdbapi.com/?t=" + filmeLink.getFilmeLink() + "&apikey=" + api.getApi()))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
 }
